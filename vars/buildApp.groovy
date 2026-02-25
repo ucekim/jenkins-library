31e10db28a3196e72b788ded6701d187f5d3e5ce
josepf <121633895+ucekim@users.noreply.github.com>
2026-02-25 10:39:03 +0300

    Update buildApp.groovy

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,23 +1,3 @@
-def call(Map config = [:]) {
-
-    pipeline {
-        agent any
-
-        stages {
-
-            stage('Build') {
-                steps {
-                    sh "echo Building ${config.appName}"
-                }
-            }
-
-            stage('Test') {
-                steps {
-                    sh "echo Testing ${config.appName}"
-                }
-            }
-
-        }
-    }
+def call(String appName) {
+    sh "echo Building ${appName}"
 }
-
